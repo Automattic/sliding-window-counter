8ed78a244083a28b56a26904a7634a58bd930f17
Alexey Kopytko <alexey@kopytko.com>
2023-06-21 17:39:55 +0900

    Revert header changes

--- example.php
+++ example.php
@@ -21,10 +21,10 @@ declare(strict_types=1);
  * The Sliding Window Counter, a short-lived time series library.
  * Copyright 2023 Automattic, Inc.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- src/SlidingWindowCounter.php
+++ src/SlidingWindowCounter.php
@@ -3,10 +3,10 @@
  * The Sliding Window Counter, a short-lived time series library.
  * Copyright 2023 Automattic, Inc.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
